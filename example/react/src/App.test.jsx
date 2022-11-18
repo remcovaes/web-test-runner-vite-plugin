888fa017cecb62e018ed1784c56f92e7f18a8f95
Remco Vaes <remcovaes@topdesk.com>
2022-11-18 15:53:52 +0100

    Fix React example

--- example/react/src/App.test.jsx
+++ example/react/src/App.test.jsx
@@ -4,10 +4,11 @@ import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    document.querySelector("[data-testid=button]").click();
-    expect(document.body.contains(linkElement));
+  it('renders learn react link', async () => {
+    const { getByText } = render(<App/>);
+    const button = document.querySelector("[data-testid=button]");
+    button.click()
+    await Promise.resolve();
+    expect(button.textContent).to.contain('1');
   });
 });
